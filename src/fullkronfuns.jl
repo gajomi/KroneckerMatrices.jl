4945002e792e81b337dd729dd70a25a355113758
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-04-15 22:39:46 +0200

    Added in various explicit kronecker type operations for Matrix types

---
+++ src/fullkronfuns.jl
@@ -0,0 +1,24 @@
+kronproduct = kron
+
+function kronpower(A::Matrix,n::Integer)
+    B = A
+    for k=1:n
+        B = kron(B,A)
+    end
+    return B
+end
+
+function kronsum(A::Matrix,B::Matrix)
+    K,L = size(A)
+    M,N = size(B)
+    K==L || error("arguments must be square matrices")
+    return kronproduct(A,diagm(ones(M)))+kronproduct(diagm(ones(K)),B)
+end
+
+function kronrepeatedsum(A::Matrix,n::Integer)
+    B = A
+    for k=1:n
+        B = kronsum(B,A)
+    end
+    return B
+end
